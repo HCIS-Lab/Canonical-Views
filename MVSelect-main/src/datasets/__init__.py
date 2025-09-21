402703c098c7a8b4c6d8ca0e62ba5e79c74bd3ca
Hank Kung <hank910140@gmail.com>
2025-09-21 16:53:04 -0400

    mvselect

---
+++ MVSelect-main/src/datasets/__init__.py
@@ -0,0 +1,5 @@
+from .wildtrack import Wildtrack
+from .multiviewx import MultiviewX
+from .frameDataset import frameDataset
+from .modelnet40 import ModelNet40
+from .scanobjectnn import ScanObjectNN
